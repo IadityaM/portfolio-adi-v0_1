4c4abd99d230e1504734af663fa1d9cb32eba626
Aditya Mishra <aditya.mishra@dynamix.co.in>
2020-02-10 22:04:40 +0530

    Updates

---
+++ precache-manifest.a7bfbbca428390076c6f73add67257bb.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3a3721cdc31ba137ba10155331415d1",
+    "url": "/portfolio-adi-v0_1/index.html"
+  },
+  {
+    "revision": "86d3563ceba50f127b21",
+    "url": "/portfolio-adi-v0_1/static/css/3.f4b2eef7.chunk.css"
+  },
+  {
+    "revision": "c77d7dbffbb95bd19e57",
+    "url": "/portfolio-adi-v0_1/static/css/4.53580411.chunk.css"
+  },
+  {
+    "revision": "8f095f910db322eb5b5e",
+    "url": "/portfolio-adi-v0_1/static/css/main.0ba72637.chunk.css"
+  },
+  {
+    "revision": "de5bf0dfb3fd8fc21fb2",
+    "url": "/portfolio-adi-v0_1/static/js/2.2d2fb1e8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio-adi-v0_1/static/js/2.2d2fb1e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86d3563ceba50f127b21",
+    "url": "/portfolio-adi-v0_1/static/js/3.1095c146.chunk.js"
+  },
+  {
+    "revision": "c77d7dbffbb95bd19e57",
+    "url": "/portfolio-adi-v0_1/static/js/4.c80e5813.chunk.js"
+  },
+  {
+    "revision": "790cbe9c0a0e910379e4",
+    "url": "/portfolio-adi-v0_1/static/js/5.c862c7dc.chunk.js"
+  },
+  {
+    "revision": "ee223283104b1d869405",
+    "url": "/portfolio-adi-v0_1/static/js/6.f2e94e0f.chunk.js"
+  },
+  {
+    "revision": "210bc1940292da5b72c9",
+    "url": "/portfolio-adi-v0_1/static/js/7.0c66f14b.chunk.js"
+  },
+  {
+    "revision": "8f095f910db322eb5b5e",
+    "url": "/portfolio-adi-v0_1/static/js/main.a044cce4.chunk.js"
+  },
+  {
+    "revision": "9eb4e2bd711e3730eccc",
+    "url": "/portfolio-adi-v0_1/static/js/runtime-main.59d9c19a.js"
+  },
+  {
+    "revision": "ca64002177cdd697644b58c73b9ec0b0",
+    "url": "/portfolio-adi-v0_1/static/media/adi_maker_launcher.ca640021.svg"
+  },
+  {
+    "revision": "9178ad6e1d4c815b5100c27de8dccce1",
+    "url": "/portfolio-adi-v0_1/static/media/android_robot.9178ad6e.svg"
+  },
+  {
+    "revision": "1c0c349ad9921e0675e6d7a2920d4c72",
+    "url": "/portfolio-adi-v0_1/static/media/angular_logo.1c0c349a.svg"
+  },
+  {
+    "revision": "00cc5bdb950c7cd82c90fbe74b26b3cf",
+    "url": "/portfolio-adi-v0_1/static/media/clang_logo.00cc5bdb.svg"
+  },
+  {
+    "revision": "8cc47c68f0d71b364d6e3b793dbb6e55",
+    "url": "/portfolio-adi-v0_1/static/media/cpp_logo.8cc47c68.svg"
+  },
+  {
+    "revision": "525f317fe9e90927edf7694d88ce42c8",
+    "url": "/portfolio-adi-v0_1/static/media/html_logo.525f317f.svg"
+  },
+  {
+    "revision": "dd60b11e9762b6e25f6d4ce981a1eeae",
+    "url": "/portfolio-adi-v0_1/static/media/js_logo_unofficial_2.dd60b11e.svg"
+  },
+  {
+    "revision": "89ccb09060a7adf788ea2340c54d6d78",
+    "url": "/portfolio-adi-v0_1/static/media/node_logo.89ccb090.svg"
+  },
+  {
+    "revision": "fbef321e03c16bcae42560f8507757d6",
+    "url": "/portfolio-adi-v0_1/static/media/python-logo-notext.fbef321e.svg"
+  },
+  {
+    "revision": "d98658a0dd3488d97e45ed3bce9d788b",
+    "url": "/portfolio-adi-v0_1/static/media/pytorch-logo-icon.d98658a0.svg"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "/portfolio-adi-v0_1/static/media/react-logo.05a3e30f.svg"
+  },
+  {
+    "revision": "6f83f74d3d9e3e2bc737f66c599156fa",
+    "url": "/portfolio-adi-v0_1/static/media/tf_logo.6f83f74d.svg"
+  }
+]);
---
+++ static/js/6.f2e94e0f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpportfolio-adi-cra"]=this["webpackJsonpportfolio-adi-cra"]||[]).push([[6],{31:function(t,e,a){"use strict";a.r(e);var i=a(0),r=a.n(i),n=a(29);e.default=function(t){var e=t.id_list,a=n.a.projects;return r.a.createElement("ul",{className:"proj_tag_list"},e&&a&&a.filter((function(t){return e.includes(t.id)})).map((function(t){return r.a.createElement("li",{key:t.id,className:"proj_tag"},t.title)})))}}}]);
+//# sourceMappingURL=6.f2e94e0f.chunk.js.map
